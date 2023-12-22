05451be951ef7addeb1ec7ca99cd7145d4ccd924
bwetmore <72516211+bwetmore@users.noreply.github.com>
2023-12-21 23:06:40 -0700

    Update SveltyPicker.svelte

--- src/lib/components/SveltyPicker.svelte
+++ src/lib/components/SveltyPicker.svelte
@@ -690,6 +690,7 @@
     gap: 0.5rem;
     justify-content: space-between;
     flex-flow: row-reverse;
+    padding-bottom: 3px; // BW
   }
   .sdt-action-btn {
     /*padding: 0.25em 0.5em;*/
@@ -700,7 +701,8 @@
   .sdt-today-btn {
     background-color: var(--sdt-bg-selected, #286090);
     color: var(--sdt-today-color, var(--sdt-bg-main, #fff));
-    padding: 0.25em 0.5em;
+    /*padding: 0.25em 0.5em;*/
+    padding: 0 3px
     font-size: 0.875em;
     border-radius: 0.2em;
     border: 1px solid var(--sdt-today-bg, #286090);
